5acfde4a476ed876af45d1783466097c5e0b1a5d
Wilfred Hughes <me@wilfred.me.uk>
2016-10-22 00:22:21 -0400

    Skeleton JSON encoding

--- rustls.el
+++ rustls.el
@@ -4,6 +4,8 @@
 
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Keywords: rust
+;; Version: 0.1
+;; Package-Requires: ((json-rpc "20160427"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -23,6 +25,28 @@
 ;; Basic integration of Emacs with rustls. Alpha.
 
 ;;; Code:
+
+(require 'ht)
+
+(defun rustls--build-request (table)
+  (let ((json-encoding-pretty-print t))
+    (json-encode table)))
+
+(defvar rustls--dir "/home/wilfred/projects/tiddles/")
+
+;; TODO: explore extending elisp-json-rpc to add pipe support in
+;; addition to HTTP.
+(defun rustls--init ()
+  (rustls--build-request
+   (ht
+    ("jsonrpc" "2.0")
+    ;; TODO: a better value?
+    ("id" 1)
+    ("method" "initialize")
+    ("params"
+     (ht ("processId" (emacs-pid))
+         ("root-path" rustls--dir)
+         ("params" (ht)))))))
 
 (provide 'rustls)
 ;;; rustls.el ends here
